a18858b48c976ce985e2e6b0b392faa7a616585d
JacobLister22 <60367192+JacobLister22@users.noreply.github.com>
2021-01-19 14:25:04 +0000

    Main-Menu
    - Began working on the menu UI
    - Added some text animations
    - Started working on UI animation

---
+++ Untitled Furniture Builder/Assets/Scripts/Menu/SequenceAnimator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SequenceAnimator : MonoBehaviour
+{
+    List<Animator> _animators;
+    public float WaitBetween = 0.1f;
+    public float WaitEnd = 3f;
+    void OnEnable()        
+    {
+        _animators = new List<Animator>(GetComponentsInChildren<Animator>());   // get reference to the animator in each child
+
+        StartCoroutine(DoAnimation());      // start the coroutine
+    }
+
+    IEnumerator DoAnimation()
+    {
+        while (true)
+        {
+            foreach (var animator in _animators)        // loop over each animator in the list of animators
+            {
+                animator.SetTrigger("DoAnim");          // set the trigger which causes the text objects to trigger their animation
+                yield return new WaitForSeconds(WaitBetween);   // wait for some time inbetween each animation
+            }
+
+            yield return new WaitForSeconds(WaitEnd);       // after each text component has animated, it waits for a few seconds until starting the coroutine again
+        }
+    }
+}
