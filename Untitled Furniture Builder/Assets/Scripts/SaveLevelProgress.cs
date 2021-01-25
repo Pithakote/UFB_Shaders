b7389aa276071899cbd02e9ac1ef7251d75fd4ac
JacobLister22 <60367192+JacobLister22@users.noreply.github.com>
2021-01-25 17:03:58 +0000

    UFB-51_Save/Load system
    + Began writing a script that takes in values and saves them in player prefs
    + created a basic UI to demonstrate how the system would work

---
+++ Untitled Furniture Builder/Assets/Scripts/SaveLevelProgress.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveLevelProgress : MonoBehaviour
+{
+    public Text levelProgText;
+    public int levelProgress;
+
+
+    public void completeLvl1()
+    {
+        levelProgress++;
+        
+    }
+    public void completeLvl2()
+    {
+        levelProgress++;
+        
+    }
+    public void completeLvl3()
+    {
+        levelProgress++;
+
+    }
+
+    public void saveProgress()
+    {
+        PlayerPrefs.SetInt("levelProgress", levelProgress);
+        PlayerPrefs.Save();
+        levelProgText.text = "Level progress = " + levelProgress;
+        Debug.Log("Your level progress is : " + PlayerPrefs.GetInt("levelProgress"));
+    }
+
+    public void loadProgress()
+    {
+        levelProgress = PlayerPrefs.GetInt("levelProgress");
+        levelProgText.text = "Level progress = " + levelProgress;
+        Debug.Log("Your level progress is : " + PlayerPrefs.GetInt("levelProgress"));
+    }
+}
+    
