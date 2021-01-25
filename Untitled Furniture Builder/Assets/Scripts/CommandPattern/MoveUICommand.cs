a09a7933bcd0a121ec65e82bfbbe93fa222ae63e
Anish Pithakote <muttsang@gmail.com>
2021-01-25 17:30:51 +0000

    Merged with the Crit01 branch and started Command Pattern

---
+++ Untitled Furniture Builder/Assets/Scripts/CommandPattern/MoveUICommand.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveUICommand : ICommand
+{
+    public MoveUICommand()
+    { 
+        
+    }
+    public void Execute()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Undo()
+    {
+        throw new System.NotImplementedException();
+    }
+
+ 
+}
