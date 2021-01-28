05a64b4e3a2f6b70504dac2a03b307f375353c01
RyanA2000 <56773739+RyanA2000@users.noreply.github.com>
2021-01-28 01:09:19 -0800

    Timer and Pause Menu
    The basic UI and function for the timer and Pause Menu has been implemented in this commit.

---
+++ Untitled Furniture Builder/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    public Animator animator;
+
+    private bool AnimatorIsPlaying()
+    {
+        return animator.GetCurrentAnimatorStateInfo(0).length > animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    private void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        GameIsPaused = false;
+        Cursor.visible = false;
+        Time.timeScale = 1;
+        MouseLook.canMove = true;
+    }
+
+    private void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        GameIsPaused = true;
+        MouseLook.canMove = false;
+        StartCoroutine(setPaused());
+    }
+
+    IEnumerator setPaused()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Time.timeScale = 0;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game..");
+        Application.Quit();
+    }
+
+    public void loadMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ Untitled Furniture Builder/Assets/Scripts/Timer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI timeText;
+    public float startTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //startTime = 60.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        startTime -= Time.deltaTime;
+        string minutes = ((int)startTime / 60).ToString();
+        string seconds = (startTime % 60).ToString("00");
+
+        timeText.text = minutes + ":" + seconds;
+
+        //need to load a different scene or UI text when timer hits 0
+        // for now it just resets
+        if (startTime == 0)
+            startTime = 60.0f;
+    }
+}
