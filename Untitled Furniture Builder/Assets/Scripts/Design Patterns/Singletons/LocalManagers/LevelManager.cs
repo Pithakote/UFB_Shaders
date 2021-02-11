48930cb0bbce16e36f70fd6b136d1796372900c9
JacobLister22 <60367192+JacobLister22@users.noreply.github.com>
2021-02-11 17:14:10 +0000

    Fixed level-manager conflict

--- Untitled Furniture Builder/Assets/Scripts/Design Patterns/Singletons/LocalManagers/LevelManager.cs
+++ Untitled Furniture Builder/Assets/Scripts/Design Patterns/Singletons/LocalManagers/LevelManager.cs
@@ -14,18 +14,6 @@ public class LevelManager : LocalManager
 	public int[] snap_tos;
 	public Vector3[] snap_offsets;
 	public GameObject[] snappables;
-<<<<<<< HEAD:Untitled Furniture Builder/Assets/Scripts/LevelManager.cs
-	
-    // Start is called before the first frame update
-    void Start()
-    {
-		Vector3 offset = new Vector3(0.5f,0,-0.5f);
-		
-		int amt_snappables = snap_tos.Length;
-		snappables = new GameObject[amt_snappables];
-		
-		for (int i = 0;  i <= amt_snappables - 1; i++)
-=======
 
 	[SerializeField]
 	GameObject _pauseMenuObject;
@@ -49,7 +37,6 @@ public class LevelManager : LocalManager
 		snappables = new GameObject[amt_snappables];
 
 		for (int i = 0; i <= amt_snappables - 1; i++)
->>>>>>> parent of a179e38 (Revert "Merge branch 'UFB-56-Design-Patterns'"):Untitled Furniture Builder/Assets/Scripts/Design Patterns/Singletons/LocalManagers/LevelManager.cs
 		{
 			float min = Random.Range(-1, 1);
 			if (min == 0)
@@ -57,26 +44,20 @@ public class LevelManager : LocalManager
 
 			snappables[i] = SpawnSnappable(snap_meshes[i], startpos + (offset * i), snap_offsets[i], i, snap_tos[i]);
 		}
-<<<<<<< HEAD:Untitled Furniture Builder/Assets/Scripts/LevelManager.cs
-		
-		Player_Managerv2.snappables = snappables;
-    } 
-=======
->>>>>>> parent of a179e38 (Revert "Merge branch 'UFB-56-Design-Patterns'"):Untitled Furniture Builder/Assets/Scripts/Design Patterns/Singletons/LocalManagers/LevelManager.cs
 
 		Player_Managerv2.snappables = snappables;
 	}
-  
-    protected override void Start()
-    {
+
+	protected override void Start()
+	{
 		//base class abstraction
 		base.Start();
 
 		InitializeSnappable();
 
-	} 
-  
-	
+	}
+
+
 	GameObject SpawnSnappable(Mesh model, Vector3 origin, Vector3 offset, int id, int snap_to)
 	{
 		GameObject ent = Instantiate(snappable);
@@ -88,25 +69,25 @@ public class LevelManager : LocalManager
 		s.snap_to_id = snap_to;
 		s.offset = offset;
 		ent.GetComponent<MeshFilter>().sharedMesh = model;
-		
+
 		MeshCollider[] colliders = ent.GetComponentsInChildren<MeshCollider>();
-		foreach ( MeshCollider col in colliders )
+		foreach (MeshCollider col in colliders)
 		{
 			col.sharedMesh = null;
 			col.sharedMesh = model;
 		}
-		
+
 		ent.GetComponent<MeshCollider>().sharedMesh = model;
 		ent.GetComponent<MeshRenderer>().material = materialShader;
-		
+
 		if (triggerMinDistance > 0)
 			s.triggerMinDistance = triggerMinDistance;
-		
+
 		s.min_angle = minAngleSnap;
 		//--
 		return ent;
 	}
-	
+
 }
 
 
