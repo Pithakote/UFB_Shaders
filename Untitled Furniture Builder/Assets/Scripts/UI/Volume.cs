2d279b5ecf5c76884f56d6c32c9c17e71b4a2bab
Pithakote <muttsang@gmail.com>
2021-02-12 18:04:47 +0000

    UFB-124 Made the volume slider work for every scene
    Made the volume slider for all scenes work with Ryan's code and work with the Singleton Audio Source.

--- Untitled Furniture Builder/Assets/Scripts/UI/Volume.cs
+++ Untitled Furniture Builder/Assets/Scripts/UI/Volume.cs
@@ -6,9 +6,17 @@ using UnityEngine.Audio;
 public class Volume : MonoBehaviour
 {
     public AudioMixer audio;
-   
+    [SerializeField]
+    AudioSource _audioSrc;
+
+    private void Start()
+    {
+        _audioSrc = GameManager.Instance.AudioManager.AudioSourceBackgroundMusic;
+    }
+
     public void MusicVolume (float volumeSlider)
     {
-        audio.SetFloat("MusicVolume", Mathf.Log10(volumeSlider) * 20);
+        // audio.SetFloat("MusicVolume", Mathf.Log10(volumeSlider) * 20);
+        _audioSrc.volume = volumeSlider;
     }
 }
