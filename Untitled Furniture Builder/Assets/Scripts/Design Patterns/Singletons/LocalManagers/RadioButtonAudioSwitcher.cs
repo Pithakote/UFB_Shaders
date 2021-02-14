8cea9194871ec26a524e103a960eb843e37a81ef
Pithakote <muttsang@gmail.com>
2021-02-14 21:12:48 +0000

    UFB-113 Added the button click effect and Emissive button
    - @JacobLister22  and I added the button click sound on the radio when it is clicked on. The click effect was found by @ti-grigorova
    
    -Added the emissive material on the button to signify that the button is interactive.
    
    Co-Authored-By: JacobLister22 <60367192+JacobLister22@users.noreply.github.com>

--- Untitled Furniture Builder/Assets/Scripts/Design Patterns/Singletons/LocalManagers/RadioButtonAudioSwitcher.cs
+++ Untitled Furniture Builder/Assets/Scripts/Design Patterns/Singletons/LocalManagers/RadioButtonAudioSwitcher.cs
@@ -9,6 +9,8 @@ public class RadioButtonAudioSwitcher : MonoBehaviour
                                                      Color.green,
                                                      Color.blue,
                                                      Color.magenta};
+
+    [SerializeField] AudioClip _radioChangeEffect;
     private void Start()
     {
         _instance = GameManager.Instance;
@@ -19,6 +21,13 @@ public class RadioButtonAudioSwitcher : MonoBehaviour
         Debug.Log(string.Format("<color=#{0:X2}{1:X2}{2:X2}>{3}</color>", (byte)(color.r * 255f), (byte)(color.g * 255f), (byte)(color.b * 255f),
                                     "Before Stopping music"));
         _instance.AudioManager.AudioSourceBackgroundMusic.Stop();
+
+        float _bufferVolume = _instance.AudioManager.AudioSourceBackgroundMusic.volume;
+
+        _instance.AudioManager.AudioSourceBackgroundMusic.volume = 1f;
+        _instance.AudioManager.AudioSourceBackgroundMusic.PlayOneShot(_radioChangeEffect);
+
+        _instance.AudioManager.AudioSourceBackgroundMusic.volume = _bufferVolume;
         _instance.AudioManager.PlayBGMusic();
         Debug.Log(string.Format("<color=#{0:X2}{1:X2}{2:X2}>{3}</color>", (byte)(color.r * 255f), (byte)(color.g * 255f), (byte)(color.b * 255f),
                                     "After Playing music"));
