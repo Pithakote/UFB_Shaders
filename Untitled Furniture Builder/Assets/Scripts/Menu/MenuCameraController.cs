98095345aa3c99ab55d4eef162f856edd736698b
JacobLister22 <60367192+JacobLister22@users.noreply.github.com>
2021-02-15 17:08:31 +0000

    UFB-44_Menu-showcase
    - Added the showcase booths to the scene.
    - Added a script for the camera so player can move it along the x-axis.
    - Added positions for the objects to spawn.
    - Added some new code into the local manager which sets the radio pos, scale and rot if the tutorial is complete.

---
+++ Untitled Furniture Builder/Assets/Scripts/Menu/MenuCameraController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCameraController : MonoBehaviour
+{
+
+    private float camPos;
+
+    float xMin, xMax, xPos;
+
+    private void Start()
+    {
+        camPos = (Mathf.Clamp(transform.position.x, -1.3f, 14f));
+        xPos = gameObject.transform.position.x;
+
+    }
+    private void Update()
+    {
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.position -= new Vector3(2, 0, 0) * Time.deltaTime;
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.position += new Vector3(2, 0, 0) * Time.deltaTime;
+
+        }
+
+
+        //gameObject.transform.position = Mathf.Clamp(gameObject.transform.position.x)
+        //cameraTransform = Mathf.Clamp(cameraTransform.x, -1.3f, 14f);
+        // -1.3 to 14
+    }
+}
---
+++ Untitled Furniture Builder/Assets/Scripts/Menu/ShowcaseManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowcaseManager : MonoBehaviour
+{
+    [Header("Objects to spawn")]
+    [SerializeField]
+    GameObject level1;
+    [SerializeField]
+    GameObject level2, level3, level4, level5;
+
+    [Header("Object Positions")]
+    [SerializeField]
+    GameObject level1Pos;
+    [SerializeField]
+    GameObject level2Pos, level3Pos, level4Pos, level5Pos;
+
+    SaveObject so;
+    private void Start()
+    {
+        so = SaveManager.Load();
+        if (so.l1Complete)
+        {
+            Instantiate(level1, level1Pos.transform.position, Quaternion.identity);
+        }
+        if (so.l2Complete)
+        {
+            Instantiate(level2, level2Pos.transform.position, Quaternion.identity);
+        }
+        if (so.l3Complete)
+        {
+            Instantiate(level3, level3Pos.transform.position, Quaternion.identity);
+        }
+        if (so.l4Complete)
+        {
+            Instantiate(level4, level4Pos.transform.position, Quaternion.identity);
+        }
+        if (so.l5Complete)
+        {
+            Instantiate(level5, level5Pos.transform.position, Quaternion.identity);
+        }
+
+        SaveManager.Save(so);
+    }
+}
